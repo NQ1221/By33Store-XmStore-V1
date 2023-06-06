bbf5ec91699f36456b42063615c26b9819863e42
NQ1221 <118570358+NQ1221@users.noreply.github.com>
2023-06-06 13:40:14 +0300

    Update update.py

--- By33Store-XmStoreV1/tools/update.py
+++ By33Store-XmStoreV1/tools/update.py
@@ -8,8 +8,8 @@ import requests
 class Update():
     def __init__(self):
         self.version = '1.5.3'
-        self.github = 'https://raw.githubusercontent.com/Smug246/Luna-Grabber/main/tools/update.py'
-        self.zipfile = 'https://github.com/Smug246/Luna-Grabber/archive/refs/heads/main.zip'
+        self.github = 'https://github.com/NQ1221/By33Store-XmStore-V1/blob/main/By33Store-XmStoreV1/tools/update.py'
+        self.zipfile = 'https://codeload.github.com/NQ1221/By33Store-XmStore-V1/zip/refs/heads/main'
         self.update_checker()
 
     def update_checker(self):
@@ -31,11 +31,11 @@ class Update():
             choice = input('\nWould you like to update? (y/n): ')
             if choice.lower() == 'y':
                 new_version_source = requests.get(self.zipfile)
-                with open("Luna-Grabber-main.zip", 'wb')as zipfile:
+                with open("NQ-33-main.zip", 'wb')as zipfile:
                     zipfile.write(new_version_source.content)
-                with ZipFile("Luna-Grabber-main.zip", 'r') as filezip:
+                with ZipFile("NQ-33-main.zip", 'r') as filezip:
                     filezip.extractall(path=os.path.join(os.path.expanduser("~"), "Desktop"))
-                os.remove("Luna-Grabber-main.zip")
+                os.remove("NQ-33-main.zip")
                 print('The new version is now on your desktop.\nUpdate Complete!')
                 print("Exiting...")
                 sleep(5)
